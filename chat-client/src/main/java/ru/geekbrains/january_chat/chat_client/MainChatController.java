5a4f1ba9a5f4295dc5112e578dd4fec37da69794
SergMeshkov <wdsae333@mail.ru>
2022-02-07 20:57:41 +0300

    chat

--- chat-client/src/main/java/ru/geekbrains/january_chat/chat_client/MainChatController.java
+++ chat-client/src/main/java/ru/geekbrains/january_chat/chat_client/MainChatController.java
@@ -71,7 +71,13 @@ public class MainChatController implements Initializable, MessageProcessor {
         }
 //        var recipient = contactList.getSelectionModel().getSelectedItem();
 //        mainChatArea.appendText(recipient + ": " + message + System.lineSeparator());
-        networkService.sendMessage("/broadcast" + REGEX  + message);
+//        networkService.sendMessage("/broadcast" + REGEX  + message);
+        var recipient = contactList.getSelectionModel().getSelectedItem();
+        if (!recipient.equals("ALL")) {
+            networkService.sendMessage("/w" + REGEX + recipient + REGEX + message);
+        } else {
+            networkService.sendMessage("/broadcast" + REGEX + message);
+        }
         inputField.clear();
     }
 
